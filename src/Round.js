13a9512e4631dfcd6e2229fe883e6e38390f9077
Liliana Weimer <lilianaweimer11@gmail.com>
2020-05-05 21:00:42 -0600

    Add some of the Round class

--- src/Round.js
+++ src/Round.js
@@ -1,10 +1,28 @@
+const Turn = require('../src/Turn');
+
 class Round {
   constructor(deck) {
-    this.deck = deck;
+    this.deck = deck.cards;
+    this.currentCard = 0;
+    this.currentTurn;
+    this.turns = 0;
+    this.incorrectGuesses = [];
   }
 
   returnCurrentCard(currentCard) {
-    return this.deck[currentCard];
+    return this.deck[this.currentCard];
+  }
+
+  takeTurn(guess) {
+    this.currentTurn = new Turn(guess, this.deck[this.currentCard]);
+    switch (this.currentTurn.evaluateGuess()) {
+      case (false):
+        this.incorrectGuesses.push(this.deck[this.currentCard].id);
+      case (true):
+        console.log(this.currentTurn.giveFeedback())
+        this.turns++;
+        this.currentCard++;
+    }
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -9,28 +9,84 @@ const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pu
 const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
 const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
 
-const testDeck = [card1, card2, card3];
+const testDeck = new Deck([card1, card2, card3]);
 
 describe('Round', function() {
 
   it('should be a function', function() {
-    const round = new Round();
+    const round = new Round(testDeck);
     expect(Round).to.be.a('function');
   });
 
   it('should be an instance of Round', function() {
-    const round = new Round();
+    const round = new Round(testDeck);
     expect(round).to.be.an.instanceof(Round);
   });
 
   it('should be initialized with a deck of cards', function() {
     const round = new Round(testDeck);
-    expect(round.deck).to.equal(testDeck);
+    expect(round.deck).to.equal(testDeck.cards);
   });
 
   it('should be able to return the current card', function() {
     const round = new Round(testDeck);
-    expect(round.returnCurrentCard()).to.equal(testDeck[0]);
+    expect(round.returnCurrentCard()).to.equal(testDeck.cards[0]);
+  });
+
+  it('should be able to take a turn', function() {
+    const round = new Round(testDeck);
+    expect(round.takeTurn).to.be.a('function');
+  });
+
+  it('should create a new Turn when a guess is made', function() {
+    const round = new Round(testDeck);
+    const turn1 = new Turn('pug', card1);
+
+    round.takeTurn('pug');
+    expect(round.currentTurn).to.deep.equal(turn1);
+  });
+
+  it('should keep track of the turn count', function() {
+    const round = new Round(testDeck);
+
+    expect(round.turns).to.equal(0);
+  });
+
+  it('should increase the turn count when a guess is made', function() {
+    const round = new Round(testDeck);
+
+    round.takeTurn('pug');
+    expect(round.turns).to.equal(1);
+  });
+
+  it('should go to the next card when a guess is made', function() {
+    const round = new Round(testDeck);
+
+    round.takeTurn('pug');
+    expect(round.currentCard).to.equal(1);
+    expect(round.returnCurrentCard()).to.equal(testDeck.cards[1])
+  });
+
+  it('should evaluate if a guess is correct', function() {
+    const round = new Round(testDeck);
+
+    round.takeTurn('pug');
+    expect(round.incorrectGuesses).to.deep.equal([1]);
+    round.takeTurn('gallbladder');
+    expect(round.incorrectGuesses).to.deep.equal([1]);
+    round.takeTurn('listening to music');
+    expect(round.incorrectGuesses).to.deep.equal([1, 12]);
+  });
+
+  it('should provide feedback about whether a guess was correct', function() {
+    const round = new Round(testDeck);
+
+    round.takeTurn('pug');
+    expect(round.takeTurn('pug')).to.equal('Incorrect!');
+    round.takeTurn('gallbladder');
+    expect(round.takeTurn('gallbladder')).to.equal('Correct!');
+    round.takeTurn('listening to music');
+    expect(round.takeTurn('listening to music')).to.equal('Incorrect!');
   });
 
 });
